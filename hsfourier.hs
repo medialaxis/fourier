06cde3841232b31ab85f11b105eacedbb36a1996
Andreas Edlund <andreas@myrstacken.net>
2016-02-08 21:21:47 +0100

    increase size of dft job to make benchmarking reasonable

--- hsfourier.hs
+++ hsfourier.hs
@@ -3,5 +3,5 @@ import qualified Data.Vector.Unboxed as VU
 import Data.Complex
 
 main = do
-    let example = VU.replicate 1024 ((1 :+ 0) :: Complex Double)
+    let example = VU.replicate (1024*6) ((1 :+ 0) :: Complex Double)
     print $ prop_inverseDft example
